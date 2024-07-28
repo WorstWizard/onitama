feebc75777bc17c4b113cdd84770a1fead81220d
WorstWizard <blobyfloby@gmail.com>
2024-07-28 17:03:30 +0200

    Switched to a fixed default for boards, with a separate function for random cards at start

--- src/game.rs
+++ src/game.rs
@@ -69,7 +69,25 @@ pub struct Board {
 }
 
 impl Default for Board {
+    /// Default board setup with no moves taken and using the cards: Boar, Cobra, Crab, Crane, Dragon
     fn default() -> Self {
+        let squares = Self::default_squares();
+        let initial_cards = [cards::BOAR, cards::COBRA, cards::CRAB, cards::CRANE, cards::DRAGON];
+        Board {
+            red_to_move: true,
+            squares,
+            red_cards: (initial_cards[0], initial_cards[1]),
+            blue_cards: (initial_cards[2], initial_cards[3]),
+            transfer_card: initial_cards[4],
+            winner: None,
+            move_history: Vec::with_capacity(20),
+            default_start: true,
+            initial_cards
+        }
+    }
+}
+impl Board {
+    pub fn random_cards() -> Self {
         let squares = Self::default_squares();
         let rand_cards = cards::random_cards();
         Board {
@@ -84,8 +102,6 @@ impl Default for Board {
             initial_cards: rand_cards
         }
     }
-}
-impl Board {
     #[rustfmt::skip]
     fn default_squares() -> [Option<Piece>; 25] {
         use Piece::*;
