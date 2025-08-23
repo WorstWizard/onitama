f9ca4b849d2727cfd8bf6088b991c2f151795d5e
WorstWizard <blobyfloby@gmail.com>
2025-08-24 01:12:38 +0200

    Disabled reporting for now

--- src/ai/min_max_v1.rs
+++ src/ai/min_max_v1.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-static mut TERMINAL_NODES: u32 = 0;
+// static mut TERMINAL_NODES: u32 = 0;
 
 pub struct MinMaxV1 {
     max_depth: u32,
@@ -12,7 +12,7 @@ impl AIOpponent for MinMaxV1 {
         mut board: Board,
         _remaining_time: Option<Duration>,
     ) -> GameMove {
-        unsafe { TERMINAL_NODES = 0 };
+        // unsafe { TERMINAL_NODES = 0 };
         let red_to_move = board.red_to_move();
         let legal_moves = board.legal_moves();
         let mut best_move = (legal_moves[0].clone(), i32::MIN);
@@ -26,7 +26,7 @@ impl AIOpponent for MinMaxV1 {
         }
         cancel_signal.store(true, Ordering::Relaxed);
 
-        println!("V1: End nodes touched: {}, eval {}", unsafe {TERMINAL_NODES}, best_move.1);
+        // println!("V1: End nodes touched: {}, eval {}", unsafe {TERMINAL_NODES}, best_move.1);
 
         best_move.0
     }
@@ -50,7 +50,7 @@ impl MinMaxV1 {
         beta: i32,
     ) -> i32 {
         if depth == self.max_depth || board.finished() {
-            unsafe { TERMINAL_NODES += 1 };
+            // unsafe { TERMINAL_NODES += 1 };
             return evaluation(board, red_to_move);
         }
         let mut best_eval = i32::MIN;
